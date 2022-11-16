d6c41a504530d7e2c897d3dc09e0f30c4d2c9f1d
notSpainChris <christopher.e.lenhardt@gmail.com>
2022-11-16 16:59:03 -0500

    Added User class and used tester class

--- main.java
+++ main.java
@@ -1,5 +1,11 @@
 public class main {
     public static void main(String [] args){
-        
+        User u1 = new User(200405087, "Christopher Lenhardt", true);
+
+        User[] userDataBase = new User[3];  //create an array of users
+
+        userDataBase[0] = u1;
+
+        System.out.println(userDataBase[0].getId());
     }
 }
