db75581ca8e5df6eb103050b21031cd3e5cdbaee
notSpainChris <christopher.e.lenhardt@gmail.com>
2022-11-16 10:41:05 -0500

    add constructor

--- bus.java
+++ bus.java
@@ -1,5 +1,9 @@
 public class bus{
     private int busRoute;
     private String[] stops;
-    
+
+    public bus(int busRoute, String[] stops){
+        this.stops = stops;
+        this.busRoute = busRoute;
+    }
 }
